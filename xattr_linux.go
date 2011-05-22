2d6db25ba2a247df6cf197930b7ecc66d09d0e87
Dave Cheney <dave@cheney.net>
2011-05-22 18:04:19 +1000

    fixed ListXattr on linux

--- xattr_linux.go
+++ xattr_linux.go
@@ -32,11 +32,18 @@ func Getxattr(path, name string) ([]byte, os.Error) {
 // Retrieves a list of names of extended attributes associated with the 
 // given path in the file system.
 func Listxattr(path string) ([]string, os.Error) {
-	buf, e := listxattr(path)
-	if e!= 0 {
+	// find size.
+	size, e := listxattr(path, nil, 0)
+	if e != 0 {
 		return nil, &XAttrError{"listxattr", path, "", os.Errno(e)}
 	}
-	return stripUserPrefix(nullTermToStrings(buf)), nil
+	buf := make([]byte, size)
+	// Read into buffer of that size.
+	read, e := listxattr(path, &buf[0], size)
+	if e != 0 {
+		return nil, &XAttrError{"listxattr", path, "", os.Errno(e)}
+	}
+	return stripUserPrefix(nullTermToStrings(buf[:read])), nil
 }
 
 // Associates name and data together as an attribute of path. 
