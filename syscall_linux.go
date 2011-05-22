5faf32cda55ad5fbf4a54a50ef88f9fbfbebfdad
Dave Cheney <dave@cheney.net>
2011-05-22 18:13:50 +1000

    Linux now working

--- syscall_linux.go
+++ syscall_linux.go
@@ -19,14 +19,14 @@ func listxattr(path string, namebuf *byte, size int) (n int, errno int) {
 	return
 }
 
-func setxattr(path string, name string, value []byte) (errno int) {
-	var _p0 unsafe.Pointer
-	if len(value) > 0 {
-		_p0 = unsafe.Pointer(&value[0])
-	} else {
-		_p0 = unsafe.Pointer(&_zero)
-	}
-	_, _, e1 := syscall.Syscall6(syscall.SYS_SETXATTR, uintptr(unsafe.Pointer(syscall.StringBytePtr(path))), uintptr(unsafe.Pointer(syscall.StringBytePtr(name))), uintptr(_p0), uintptr(len(value)), 0, 0)
+func setxattr(path string, name string, value *byte, size int) (errno int) {
+	_, _, e1 := syscall.Syscall6(syscall.SYS_SETXATTR, uintptr(unsafe.Pointer(syscall.StringBytePtr(path))), uintptr(unsafe.Pointer(syscall.StringBytePtr(name))), uintptr(unsafe.Pointer(value)), uintptr(size), 0, 0)
+	errno = int(e1)
+	return
+}
+
+func removexattr(path string, name string) (errno int) {
+	_, _, e1 := syscall.Syscall(syscall.SYS_REMOVEXATTR, uintptr(unsafe.Pointer(syscall.StringBytePtr(path))), uintptr(unsafe.Pointer(syscall.StringBytePtr(name))), 0)
 	errno = int(e1)
 	return
 }
