7a109969f32dbc3909753882d0ed3b4f480bc1bc
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2013-01-29 11:48:58 +0400

    Fix for Darwin (Mac).

--- syscall_darwin.go
+++ syscall_darwin.go
@@ -5,22 +5,58 @@ import (
 	"unsafe"
 )
 
-func getxattr(path string, name string, value *byte, size int, pos int, options int) (int, error) {
-	r0, _, e1 := syscall.Syscall6(syscall.SYS_GETXATTR, uintptr(unsafe.Pointer(syscall.StringBytePtr(path))), uintptr(unsafe.Pointer(syscall.StringBytePtr(name))), uintptr(unsafe.Pointer(value)), uintptr(size), uintptr(pos), uintptr(options))
-	return int(r0), e1
+// http://www.opensource.apple.com/source/xnu/xnu-2050.18.24/bsd/kern/syscalls.master
+
+var noError = syscall.Errno(0)
+
+// user_ssize_t getxattr(user_addr_t path, user_addr_t attrname, user_addr_t value, size_t size, uint32_t position, int options);
+func getxattr(path string, attrname string, value *byte, size int, position uint32, options int) (resSize int, e error) {
+        r0, _, e1 := syscall.Syscall6(syscall.SYS_GETXATTR,
+                uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
+                uintptr(unsafe.Pointer(syscall.StringBytePtr(attrname))),
+                uintptr(unsafe.Pointer(value)),
+                uintptr(size), uintptr(position), uintptr(options))
+        resSize = int(r0)
+        if e1 != noError {
+                e = e1
+        }
+        return
 }
 
-func listxattr(path string, namebuf *byte, size int, options int) (int, error) {
-	r0, _, e1 := syscall.Syscall6(syscall.SYS_LISTXATTR, uintptr(unsafe.Pointer(syscall.StringBytePtr(path))), uintptr(unsafe.Pointer(namebuf)), uintptr(size), uintptr(options), 0, 0)
-	return int(r0), e1
+// user_ssize_t listxattr(user_addr_t path, user_addr_t namebuf, size_t bufsize, int options);
+func listxattr(path string, namebuf *byte, bufsize int, options int) (resSize int, e error) {
+        r0, _, e1 := syscall.Syscall6(syscall.SYS_LISTXATTR,
+                uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
+                uintptr(unsafe.Pointer(namebuf)),
+                uintptr(bufsize), uintptr(options), 0, 0)
+        resSize = int(r0)
+        if e1 != noError {
+                e = e1
+        }
+        return
 }
 
-func setxattr(path string, name string, value *byte, size int, pos int, options int) error {
-	_, _, e1 := syscall.Syscall6(syscall.SYS_SETXATTR, uintptr(unsafe.Pointer(syscall.StringBytePtr(path))), uintptr(unsafe.Pointer(syscall.StringBytePtr(name))), uintptr(unsafe.Pointer(value)), uintptr(size), uintptr(pos), uintptr(options))
-	return e1
+// int setxattr(user_addr_t path, user_addr_t attrname, user_addr_t value, size_t size, uint32_t position, int options);
+func setxattr(path string, attrname string, value *byte, size int, position uint32, options int) (e error) {
+        _, _, e1 := syscall.Syscall6(syscall.SYS_SETXATTR,
+                uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
+                uintptr(unsafe.Pointer(syscall.StringBytePtr(attrname))),
+                uintptr(unsafe.Pointer(value)),
+                uintptr(size), uintptr(position), uintptr(options))
+        if e1 != noError {
+                e = e1
+        }
+        return
 }
 
-func removexattr(path string, name string, options int) error {
-	_, _, e1 := syscall.Syscall(syscall.SYS_REMOVEXATTR, uintptr(unsafe.Pointer(syscall.StringBytePtr(path))), uintptr(unsafe.Pointer(syscall.StringBytePtr(name))), uintptr(options))
-	return e1
+// int removexattr(user_addr_t path, user_addr_t attrname, int options);
+func removexattr(path string, attrname string, options int) (e error) {
+        _, _, e1 := syscall.Syscall(syscall.SYS_REMOVEXATTR,
+                uintptr(unsafe.Pointer(syscall.StringBytePtr(path))),
+                uintptr(unsafe.Pointer(syscall.StringBytePtr(attrname))),
+                uintptr(options))
+        if e1 != noError {
+                e = e1
+        }
+        return
 }
--- xattr_darwin.go
+++ xattr_darwin.go
@@ -2,40 +2,53 @@ package xattr
 
 // Retrieve extended attribute data associated with path.
 func Getxattr(path, name string) ([]byte, error) {
-	// find size.
+        // find size
 	size, err := getxattr(path, name, nil, 0, 0, 0)
 	if err != nil {
 		return nil, &XAttrError{"getxattr", path, name, err}
 	}
+        if size == 0 {
+                return []byte{}, nil
+        }
+
+        // read into buffer of that size
 	buf := make([]byte, size)
-	// Read into buffer of that size.
-	read, err := getxattr(path, name, &buf[0], size, 0, 0)
+        size, err = getxattr(path, name, &buf[0], size, 0, 0)
 	if err != nil {
 		return nil, &XAttrError{"getxattr", path, name, err}
 	}
-	return buf[:read], nil
+        return buf[:size], nil
 }
 
-// Retrieves a list of names of extended attributes associated with the 
+// Retrieves a list of names of extended attributes associated with the
 // given path in the file system.
 func Listxattr(path string) ([]string, error) {
-	// find size.
+        // find size
 	size, err := listxattr(path, nil, 0, 0)
 	if err != nil {
 		return nil, &XAttrError{"listxattr", path, "", err}
 	}
+        if size == 0 {
+                return []string{}, nil
+        }
+
+        // read into buffer of that size
 	buf := make([]byte, size)
-	// Read into buffer of that size.
-	read, err := listxattr(path, &buf[0], size, 0)
+        size, err = listxattr(path, &buf[0], size, 0)
 	if err != nil {
 		return nil, &XAttrError{"listxattr", path, "", err}
 	}
-	return nullTermToStrings(buf[:read]), nil
+        return nullTermToStrings(buf[:size]), nil
 }
 
-// Associates name and data together as an attribute of path. 
+// Associates name and data together as an attribute of path.
 func Setxattr(path, name string, data []byte) error {
-	if err := setxattr(path, name, &data[0], len(data), 0, 0); err != nil {
+        l := len(data)
+        var p *byte
+        if l != 0 {
+                p = &data[0]
+        }
+        if err := setxattr(path, name, p, l, 0, 0); err != nil {
 		return &XAttrError{"setxattr", path, name, err}
 	}
 	return nil
